cd3cca0a25076ddbfeb7f09feed1cb230081676c
Saaad <saaadii.mahmood.7@gmail.com>
2021-10-21 19:05:46 +0500

    sb-admin theme in middle

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,8 +9,18 @@
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "bootstrap"
+import "jquery"
 import "channels"
 import "@hotwired/turbo-rails"
+
+import "./css/fontawesome/css/all.min.css"
+import "./css/sb-admin-2.min.css"
+
+import "./bootstrap-bundle/bootstrap.bundle.min"
+import "./sb-admin-2.min"
+
+import "./jquery-easing/jquery.easing.min"
+import "./sb-admin-2.min"
 
 Rails.start()
 ActiveStorage.start()
