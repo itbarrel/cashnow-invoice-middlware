59dbed61c6f13a78672eb518d3d7b149ac7b979f
Saaad <saaadii.mahmood.7@gmail.com>
2021-11-05 16:51:13 +0500

    Foreman docker settings

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,18 @@
 require 'sidekiq'
 require 'sidekiq/web'
 
+sidekiq_config = { url: ENV['REDIS_URL'] }
+
 Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
   [user, password] == ["admin", "12345678"]
 end
 
 Sidekiq.default_worker_options = { retry: 0 }
+
+Sidekiq.configure_server do |config|
+  config.redis = sidekiq_config
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = sidekiq_config
+end
--- start-rails-development-server.sh
+++ start-rails-development-server.sh
@@ -1,2 +1,3 @@
 echo 'starting Rails server ...' && rm -f tmp/pids/server.pid && bundle exec rails s -b 0.0.0.0
 echo 'Starting webpack dev server ...' && bin/webpack --watch --colors --progress
+echo 'Starting Sidekiq server ...' && bundle exec sidekiq
