bce94c96d37c2dfdf62f5306992cfcbbe7580911
Saaad <saaadii.mahmood.7@gmail.com>
2021-11-05 21:18:11 +0500

    action cable production setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,8 +42,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://invoice.cashnow.pk/cable'
+  config.action_cable.allowed_request_origins = [ 'http://invoice.cashnow.pk', /http:\/\/invoice.*/, /https:\/\/invoice.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
