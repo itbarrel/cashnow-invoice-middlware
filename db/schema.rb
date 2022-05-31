7ef2012849fb213feab350be9b28fc0d66368d09
Mishal zareen <mishalzareen153@gmail.com>
2022-05-31 17:37:29 +0500

    devise links reworked

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_203_124_931) do
+ActiveRecord::Schema.define(version: 2022_02_03_124931) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'pgcrypto'
   enable_extension 'plpgsql'
